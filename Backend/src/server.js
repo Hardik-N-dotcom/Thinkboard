99cb2a54b974ed8978a9933e370136f766cb997c
Hardik-N-dotcom <hrdknarayan@gmail.com>
2025-07-11 00:10:53 +0530

    prepared for the deployement

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,17 +4,30 @@ import NotesRoutes from "./routes/NotesRoutes.js"
 
 import connectdb from "./config/Connect.js"
 import dotenvx from "@dotenvx/dotenvx"
+import path from 'path'
 dotenvx.config()
 
 
 const app=express()
-app.use(cors());
+const PORT=process.env.PORT||5000    
+const __dirname=path.resolve()                
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors({
+    origin: "http://localhost:5173",
+    
+}));
+}
 
 app.use(express.json())
 
 app.use('/api/notes',NotesRoutes)
-const PORT=process.env.PORT||5000                    
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
 
+}
 
 
 connectdb().then(()=>{
